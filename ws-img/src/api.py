9af654d52845a0105aaa59271b80b83f5d3f56c1
Deyveson Willian <deyvesonn@outlook.com>
2019-10-23 14:40:57 -0400

    Clean code

--- ws-img/src/api.py
+++ ws-img/src/api.py
@@ -21,13 +21,67 @@ except FileNotFoundError:
     print('Profile não existe')
     sys.exit(4)
 
-# # MONGODB
-# myclient = pymongo.MongoClient(properties['mongoAddr'])
-# mybd = myclient["bd_img"]
-# mycol = mybd["Imagens"]
+# CONNECT MONGODB
+myclient = pymongo.MongoClient(properties['mongoAddr'])
+mybd = myclient["bd_img"]
+mycol = mybd["Imagens"]
 
 @app.route("/test", methods=['GET', 'POST'])
 def hello():
-    # dado = {"_id": 1, "name": "test", "base64": "asdasdasdsadasd"}
-    # x = mycol.insert(dado)
+    dado = {"_id": 2, "name": "test", "base64": "asdasdasdsadasd"}
+    x = mycol.insert(dado)
     return "Sucess"
+
+@app.route("/findImage", methods=['GET', 'POST'])
+def searchImage(codigo: int, name: str):
+
+    data = buscaImg(codigo, name)
+    response = {}
+
+    if len(data) > 0:
+        
+        return jsonify(data)
+
+    elif len(data) == 0: 
+       
+        response = compactTransformImg(codigo, name);
+        jsonify(response)
+
+    else:
+        response["menssage"] = "Imagem não existe no servidor"
+        return jsonify(response)    
+
+
+def buscaImg(codigo: int, name: str):
+    query = {"_id": "{}".format(codigo),
+             "name": "{}".format(name)}
+    return mycol.find_one(query, {'_id': 0})
+
+
+def compactTransformImg(codigo: int, name: str):
+
+    arquivo = properties['diretorio'] + name + "/" + codigo + ".JPG"
+    basewidth = 400
+    img = Image.open(arquivo)
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    new_img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    new_img.save(properties['diretorio'] + name + "/newImg" + codigo + ".JPG", optimize=True)
+
+    new_arquivo = properties['diretorio'] + name + "/newImg" + codigo + ".JPG"
+
+    f = open(new_arquivo, 'rb')
+    imgCompact = f.read()
+    f.close()
+
+    encodedImg = base64.b64encode(imgCompact)
+
+    documento = {"_id": codigo, "name": name,
+                    "base64": "{}".format(encodedImg).replace("b'", "").replace("'", "")}
+    x = mycol.insert_one(documento)
+
+    os.remove(new_arquivo)
+
+    response = buscaImg(codigo, name)
+
+    return response
